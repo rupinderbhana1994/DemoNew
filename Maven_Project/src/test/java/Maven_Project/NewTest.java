16ecef5dc1a36bf6558f5f0c2d98520df44d736d
TANU <TANU@DESKTOP-9DVV25U>
2021-04-21 19:31:59 -0400

    Maven P

---
+++ Maven_Project/src/test/java/Maven_Project/NewTest.java
@@ -0,0 +1,19 @@
+package Maven_Project;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
